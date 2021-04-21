c5f7701c77a615d3bb323c9ef1c6cf8f36059f36
Sumanta Mukherjee <sumanta.mukherjee.kat@gmail.com>
2021-04-21 18:41:10 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import "./App.scss";
 
 const App = () => {
-    return <h1> Hello world from React biolerplate </h1>;
+    return <h1> Hello world from React biolerplate - manually setup. </h1>;
 };
 
 export default App;
